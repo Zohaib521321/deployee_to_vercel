3b4e1734ecb59dd075f4935e86be02b9c4db3a9a
zohaib521321 <khanzohaib3231@gmail.com>
2025-03-02 17:48:21 +0500

    remove

--- fastapi_practice/main.py
+++ fastapi_practice/main.py
@@ -7,5 +7,4 @@ app = FastAPI()
 def home():
     return {"message": "Hello, FastAPI with Poetry!"}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
